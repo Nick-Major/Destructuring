6ac75e26597f6f317cdca8c57c0d9db7993c09f1
Nick-Major <divanchik26@gmail.com>
2024-10-29 00:04:11 +0300

    Added jest

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,37 @@
-const path = require('path');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
+import { resolve } from 'path';
+import HtmlWebPackPlugin from 'html-webpack-plugin';
 
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'app.bundle.js',
-    },
-
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                  loader: 'babel-loader',
-                },
-            },
-            {
-                test: /\.html$/,
-                use: [
-                  {
-                    loader: 'html-loader',
-                  },
-                ],
-            },
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-        ],
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-          template: './src/index.html',
-          filename: './index.html',
-        }),
-    ],
+export const entry = './src/index.js';
+export const output = {
+  path: resolve(__dirname, 'dist'),
+  filename: 'app.bundle.js',
 };
+export const module = {
+  rules: [
+    {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+      },
+    },
+    {
+      test: /\.html$/,
+      use: [
+        {
+          loader: 'html-loader',
+        },
+      ],
+    },
+    {
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader'],
+    },
+  ],
+};
+export const plugins = [
+  new HtmlWebPackPlugin({
+    template: './src/index.html',
+    filename: './index.html',
+  }),
+];
