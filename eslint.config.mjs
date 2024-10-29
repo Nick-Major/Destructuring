f22ea7aed591c67b0f23f7b37cba271ced05401f
Nick-Major <divanchik26@gmail.com>
2024-10-29 10:59:13 +0300

    Changed json and config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,5 +4,6 @@ import pluginJs from "@eslint/js";
 
 export default [
   {languageOptions: { globals: globals.node }},
+  {ignores: ["**/temp.js", "config/*"]},
   pluginJs.configs.recommended,
 ];
--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,41 @@
-import { resolve } from 'path';
-import HtmlWebPackPlugin from 'html-webpack-plugin';
+const path = require('node:path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
-export const entry = './src/index.js';
-export const output = {
-  path: resolve(__dirname, 'dist'),
-  filename: 'app.bundle.js',
-};
-export const module = {
-  rules: [
-    {
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-      },
-    },
-    {
-      test: /\.html$/,
-      use: [
-        {
-          loader: 'html-loader',
+module.exports = {
+  entry: './src/index.js',
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
         },
-      ],
-    },
-    {
-      test: /\.css$/i,
-      use: ['style-loader', 'css-loader'],
-    },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
   ],
 };
-export const plugins = [
-  new HtmlWebPackPlugin({
-    template: './src/index.html',
-    filename: './index.html',
-  }),
-];
